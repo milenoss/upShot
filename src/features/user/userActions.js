813a4fe37e987f85cd955b9513bfaf46057f41fb
milenoss <milenkhanal@gmail.com>
2019-12-22 23:22:52 +0000

    Photo upload method done

--- src/features/user/userActions.js
+++ src/features/user/userActions.js
@@ -1,4 +1,5 @@
 import { toastr } from "react-redux-toastr";
+import { asyncActionStart, asyncActionFinish, asyncActionError } from "../async/asyncActions";
 
 export const updateProfile = (user) => 
 async ( dispatch, getState, {getFirebase})=> {
@@ -12,3 +13,51 @@ async ( dispatch, getState, {getFirebase})=> {
         console.log(error)
     }
 }
+
+export const uploadProfileImage = (file, fileName) => (
+    async (dispatch, getState, {getFirebase, getFirestore}) => { 
+        const firebase = getFirebase(); 
+        const firestore = getFirestore(); 
+        const user = firebase.auth().currentUser; 
+        const path = `${user.uid}/user_images`;
+        const options = { 
+            name: fileName
+        
+        };
+        try { 
+
+            dispatch(asyncActionStart())
+            //upload the file to firebase storage
+            let uploadedFile = await firebase.uploadFile(path, file, null, options)
+            //get url of image
+            let downloadURL = await uploadedFile.uploadTaskSnapshot.ref.getDownloadURL();
+            // get userdoc 
+            let userDoc = await firestore.get(`users/${user.uid}`)
+            // check if user has photo, if not update profile
+            if(!userDoc.data().photoURL) { 
+                await firebase.updateProfile({
+                    photoURL: downloadURL
+                })
+
+                await user.updateProfile({
+                    photoURL: downloadURL
+                })
+            }
+            //add the image to firestore
+            await firestore.add({
+                collection: 'users', 
+                doc: user.uid, 
+                subcollections: [{collection: 'photos'}]
+            },{
+                name: fileName, 
+                url: downloadURL
+            })
+            dispatch(asyncActionFinish())
+            // add the image to firestore
+        } catch (error) {
+            console.log(error)
+            dispatch(asyncActionError())
+        }
+
+    }
+)
