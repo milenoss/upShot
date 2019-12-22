46ba9271aa3bf60bcbec473b6496895be5d967fe
milenoss <milenkhanal@gmail.com>
2019-12-22 22:20:46 +0000

    react cropper done

--- src/features/user/Settings/Photos/PhotosPage.jsx
+++ src/features/user/Settings/Photos/PhotosPage.jsx
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import {Image, Segment, Header, Divider, Grid, Button, Card} from 'semantic-ui-react';
 import DropzoneInput from './DropzoneInput'
+import CropperInput from './CropperInput';
 
 const PhotosPage = () => {
     const [files, setFiles] = useState([]);
+    const [image, setImage] = useState(null);
+
     //useEffect will revoke the file object.
     useEffect(()=> {
         return() => { 
@@ -23,12 +26,16 @@ const PhotosPage = () => {
                     <Grid.Column width={1} />
                     <Grid.Column width={4}>
                         <Header sub color='teal' content='Step 2 - Resize image' />
+                        {files.length > 0 &&
+                        <CropperInput setImage={setImage} imagePreview={files[0].preview}/>}
                     </Grid.Column>
                     <Grid.Column width={1} />
                     <Grid.Column width={4}>
                         <Header sub color='teal' content='Step 3 - Preview & Upload'/> 
                         {files.length > 0 &&
-                        <Image src={files[0].preview} style={{minHeight: '200px', minWidth: '200px'}} />
+                        <div
+                        className = 'img-preview'
+                        style={{minHeight: '200px', minWidth: '200px', overflow: 'hidden'}} />
 }
                     </Grid.Column>
 
