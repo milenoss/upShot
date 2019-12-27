537d86b725d50cdb8601a95a1885b8d7623b50b1
milenoss <milenkhanal@gmail.com>
2019-12-27 17:28:00 +0000

    Userphotos working

--- src/features/user/Settings/Photos/CropperInput.js
+++ src/features/user/Settings/Photos/CropperInput.js
@@ -1,40 +1,36 @@
-import React, {Component, createRef} from 'react';
+import React, { Component } from 'react';
 import Cropper from 'react-cropper';
 import 'cropperjs/dist/cropper.css';
 
 class CropperInput extends Component {
-  cropper = createRef();
+  cropImage = () => {
+      const {setImage, setCropResult} = this.props;
+      if (typeof this.refs.cropper.getCroppedCanvas() === 'undefined') {
+          return;
+      }
 
-  cropIMage = () => { 
-      const {setImage} = this.props;
-      if(typeof this.cropper.current.getCroppedCanvas() === 'undefined'){
-      return;
-  }
+      this.refs.cropper.getCroppedCanvas().toBlob(blob => {
+          setCropResult(URL.createObjectURL(blob));
+          setImage(blob)
+      })
+  };
 
-      this.cropper.current.getCroppedCanvas().toBlob(blob => {
-      setImage(blob)
-  }, 'image/jpeg')
-
-
-  }
- 
   render() {
-      const {imagePreview} = this.props;
+    const { imagePreview } = this.props;
     return (
       <Cropper
-        ref={this.cropper}
+        ref='cropper'
         src={imagePreview}
-        style={{height: 200, width: '100%'}}
-        preview ='.img-preview'
+        style={{ height: 200, width: '100%' }}
         aspectRatio={1}
-        viewMode={1}
+        viewMode={0}
         dragMode='move'
         guides={false}
         scalable={true}
         cropBoxMovable={true}
         cropBoxResizable={true}
-
-        crop={this.cropImage} />
+        crop={this.cropImage}
+      />
     );
   }
 }
